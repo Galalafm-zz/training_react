9d8c0a47059cd0ee70e75b04bae12958624e8555
Galalafm <gala.pill@gmail.com>
2018-03-26 20:59:06 +0200

    add methods todostore

---
+++ src/TodoStore.ts
@@ -0,0 +1,39 @@
+import { Todo } from './Interfaces'
+
+export default class TodoStore {
+
+  private static i = 0
+  public todos: Todo[]
+
+  static increment () {
+    return this.i++
+  }
+
+  addTodo (title: string): void {
+    this.todos = [{
+      id: TodoStore.increment(),
+      title: title,
+      completed: false
+    }, ...this.todos]
+  }
+
+  removeTodo (todo: Todo): void {
+    this.todos = this.todos.filter(t => t !== todo)
+  }
+
+  toggleTodo (todo: Todo, completed = true): void {
+    this.todos = this.todos.map(t => t === todo ? { ...t, completed } : t)
+  }
+
+  toggleAll (completed = true) {
+    this.todos = this.todos.map(t => com)
+  }
+
+  updateTitle (todo: Todo, title: string): void {
+    this.todos = this.todos.map(t => t === todo ? { ...t, title } : t)
+  }
+
+  cleanTodo (): void {
+    this.todos = this.todos.filter(t => !t.completed)
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,10 @@
-document.body.innerHTML = "Yo"
+import * as React from 'react'
+import { render } from 'react-dom'
+import s from './TodoStore'
+
+console.log(s)
+
+render(
+  <div className='header'>Helloo</div>,
+  document.getElementById('app') as Element
+)
